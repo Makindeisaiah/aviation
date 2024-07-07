46e91e25202e3fda6ac75158bb988cab189ed56f
Makinde Isaiah <makindeisaiah2002@gmail.com>
2024-07-07 21:20:44 +0100

    fix some changes in the header

---
+++ includes/header.php
@@ -0,0 +1,88 @@
+<header class="main-header-three">
+            <div class="main-header-three__top">
+                <div class="main-header-three__top-inner">
+                    <div class="main-header-three__top-left">
+                        <ul class="list-unstyled main-header-three__contact-list">
+                            <li>
+                                <div class="icon">
+                                    <i class="fas fa-map-marker-alt"></i>
+                                </div>
+                                <div class="text">
+                                    <p>Ikeja, Lagos, Nigeria</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="icon">
+                                    <i class="fas fa-envelope"></i>
+                                </div>
+                                <div class="text">
+                                    <p><a href="mailto:needhelp@company.com">contact@jetfly.com</a></p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="main-header-three__top-right">
+                        <div class="main-header-three__social">
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-facebook"></i></a>
+                            <!-- <a href="#"><i class="fab fa-pinterest-p"></i></a> -->
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <nav class="main-menu main-menu-three">
+                <div class="main-menu-three__wrapper">
+                    <div class="main-menu-three__wrapper-inner">
+                        <div class="main-menu-three__left">
+                            <div class="main-menu-three__logo">
+                                <a href="index3.html"><img src="assets/images/resources/logo-2.png" alt=""></a>
+                            </div>
+                        </div>
+                        <div class="main-menu-three__main-menu-box">
+                            <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
+                            <ul class="main-menu__list">
+                                <li class="dropdown current megamenu">
+                                    <a href="index3.html">Home </a>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#">Pages</a>
+                                    <ul class="shadow-box">
+                                        <li><a href="about.html">About</a></li>
+                                        <li><a href="team.html">Team</a></li>
+                                        <li><a href="destinations.html">Destinations</a></li>
+                                        <li><a href="destination-details.html">Destination Details</a></li>
+                                        <li><a href="testimonials.html">Testimonials</a></li>
+                                        <li><a href="gallery.html">Gallery</a></li>
+                                        <li><a href="faq.html">FAQs</a></li>
+                                        <li><a href="404.html">404 Error</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#">Services</a>
+                                    <ul class="shadow-box">
+                                        <li><a href="services.html">Services</a></li>
+                                        <li><a href="business-charter.html">Business Charter</a></li>
+                                        <li><a href="private-charter.html">Private Charter</a></li>
+                                        <li><a href="jet-rentals.html">Jet Rentals</a></li>
+                                        <li><a href="high-profile-people.html">High Profile People</a></li>
+                                        <li><a href="music-tours.html">Music Tours</a></li>
+                                        <li><a href="sports-teams.html">Sports Teams</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#">News</a>
+                                    <ul class="shadow-box">
+                                        <li><a href="news.html">News</a></li>
+                                        <li><a href="news-details.html">News Details</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="contact.html">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
