4f38f07998517b5d909e5e492c0ffe864286e032
Makinde Isaiah <makindeisaiah2002@gmail.com>
2024-07-08 08:25:50 +0100

    rearranging the header

--- includes/header.php
+++ includes/header.php
@@ -1,57 +1,25 @@
-<header class="main-header-three">
-            <div class="main-header-three__top">
-                <div class="main-header-three__top-inner">
-                    <div class="main-header-three__top-left">
-                        <ul class="list-unstyled main-header-three__contact-list">
-                            <li>
-                                <div class="icon">
-                                    <i class="fas fa-map-marker-alt"></i>
-                                </div>
-                                <div class="text">
-                                    <p>Ikeja, Lagos, Nigeria</p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="icon">
-                                    <i class="fas fa-envelope"></i>
-                                </div>
-                                <div class="text">
-                                    <p><a href="mailto:needhelp@company.com">contact@jetfly.com</a></p>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="main-header-three__top-right">
-                        <div class="main-header-three__social">
-                            <a href="#"><i class="fab fa-twitter"></i></a>
-                            <a href="#"><i class="fab fa-facebook"></i></a>
-                            <!-- <a href="#"><i class="fab fa-pinterest-p"></i></a> -->
-                            <a href="#"><i class="fab fa-instagram"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <nav class="main-menu main-menu-three">
-                <div class="main-menu-three__wrapper">
-                    <div class="main-menu-three__wrapper-inner">
-                        <div class="main-menu-three__left">
-                            <div class="main-menu-three__logo">
-                                <a href="index3.html"><img src="assets/images/resources/logo-2.png" alt=""></a>
+<header class="main-header">
+            <nav class="main-menu">
+                <div class="main-menu__wrapper">
+                    <div class="main-menu__wrapper-inner">
+                        <div class="main-menu__left">
+                            <div class="main-menu__logo">
+                                <a href="index3.php"><img src="assets/images/resources/logo-1.png" alt=""></a>
                             </div>
                         </div>
-                        <div class="main-menu-three__main-menu-box">
+                        <div class="main-menu__main-menu-box">
                             <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
                             <ul class="main-menu__list">
-                                <li class="dropdown current megamenu">
-                                    <a href="index3.html">Home </a>
+                                <li class="dropdown megamenu">
+                                    <a href="index3.php">Home </a>
                                 </li>
-                                <li class="dropdown">
+                                <li class="dropdown current">
                                     <a href="#">Pages</a>
                                     <ul class="shadow-box">
-                                        <li><a href="about.html">About</a></li>
+                                        <li><a href="about.php">About</a></li>
                                         <li><a href="team.html">Team</a></li>
                                         <li><a href="destinations.html">Destinations</a></li>
-                                        <li><a href="destination-details.html">Destination Details</a></li>
+                                        <li><a href="destination-details.php">Destination Details</a></li>
                                         <li><a href="testimonials.html">Testimonials</a></li>
                                         <li><a href="gallery.html">Gallery</a></li>
                                         <li><a href="faq.html">FAQs</a></li>
@@ -62,6 +30,7 @@
                                     <a href="#">Services</a>
                                     <ul class="shadow-box">
                                         <li><a href="services.html">Services</a></li>
+                                        <!-- <li><a href="services-carousel.html">Service Carousel</a></li> -->
                                         <li><a href="business-charter.html">Business Charter</a></li>
                                         <li><a href="private-charter.html">Private Charter</a></li>
                                         <li><a href="jet-rentals.html">Jet Rentals</a></li>
@@ -81,6 +50,25 @@
                                     <a href="contact.html">Contact</a>
                                 </li>
                             </ul>
+                        </div>
+                        <div class="main-menu__right">
+                            <div class="main-menu__search-cart-call-box">
+                                <div class="main-menu__search-cart-box">
+                                    <div class="main-menu__search-box">
+                                        <a href="#" class="main-menu__search search-toggler icon-magnifying-glass"></a>
+                                    </div>
+                                </div>
+                                <div class="main-menu__call">
+                                    <div class="main-menu__call-icon">
+                                        <i class="fas fa-phone"></i>
+                                    </div>
+                                    <div class="main-menu__call-content">
+                                        <p class="main-menu__call-sub-title">Call Anytime</p>
+                                        <h5 class="main-menu__call-number"><a href="tel:9288006780">+92 ( 8800 ) -
+                                                6780</a></h5>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
